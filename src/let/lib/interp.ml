513011b6d82057fdf99b13a916d80f619e306cec
Nathaniel Escaro <nescaro@stevens.edu>
2024-02-15 01:35:42 +0000

    Implemented Untuple

--- src/let/lib/interp.ml
+++ src/let/lib/interp.ml
@@ -35,22 +35,13 @@ open Parser_plaf.Ast
 open Parser_plaf.Parser
 open Ds
 
-(** PROVIDED IN ASSIGNMENT PDF **)
-
-(*
-eval_exprs : expr list -> (exp_val list) ea_result =
-    fun es ->
-    match es with
-    | [] -> return []
-    | h::t -> eval_expr h >>= fun i ->
-              eval_exprs t >>= fun l ->
-              return (i::l)
-*)
-(** END **)
-
 let list_of_expval : exp_val -> (exp_val list) ea_result = function
     | ListVal l -> return l
     | _ -> error "Expected a list!"
+
+let expr_of_tuple : expr -> (expr list) ea_result = function
+    | Tuple tup -> return tup
+    | _ -> error "Expected a tuple!"
 
 (** [eval_expr e] evaluates expression [e] *)
 let rec eval_expr : expr -> exp_val ea_result =
@@ -141,7 +132,11 @@ let rec eval_expr : expr -> exp_val ea_result =
   | Tuple(es) ->
     eval_exprs es >>= fun tup ->
     return (TupleVal tup)
-  | Untuple(ids, e1, e2) -> failwith ""
+  | Untuple(ids, e1, e2) ->
+    expr_of_tuple e1 >>= fun tup ->
+    eval_expr @@
+    List.fold_right2 (fun idi edi last -> Let(idi, edi, last))
+        ids tup (e2)
 
   (** ADDITIONS END HERE **)
 
